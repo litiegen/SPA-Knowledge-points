5ea58d9598443811ac5ffbda253d384b49127edd
litiegen <517206296@qq.com>
2020-03-24 20:53:55 +0800

    构建矩形计算器

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/* global module: true */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        expand: true,
+        src: ['*.html', '??-*/*.html', '??-*/??-*/*.html'],
+        dest: 'dist/'
+      }
+    },
+    cssmin: {
+      files: {   
+        expand: true,
+        src: ['css/*.css', '??-*/*.css', '??-*/??-*/*.css', '??-*/css/*.css'],
+        dest: 'dist/'
+      }
+    },
+    uglify: {
+      main: {
+        files: [{
+          expand: true,
+          src: ['js/*.js', '??-*/*.js', '??-*/0?-*/*.js', '??-*/js/*.js'],
+          dest: 'dist/'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  grunt.registerTask('default', ['uglify','cssmin', 'htmlmin']);
+};
---
+++ js/data.js
@@ -0,0 +1,128 @@
+var spaDemoData = [
+    {
+      'class': '第一个 Web app',
+      'baseUrl': '矩形计算器',
+      'demos': [{
+        'title': '矩形计算器',
+        'url': 'index.html'
+      }]
+    },
+    {
+      'class': 'H5 内置控件',
+      'baseUrl': '02-html-widget',
+      'demos': [
+        {
+          'title': '按钮事件处理',
+          'url': '05-button.html'
+        },
+        {
+          'title': '定时器按钮',
+          'url': '04-button.html'
+        },
+        {
+          'title': '按钮类控件',
+          'url': '01-button.html'
+        },
+        {
+          'title': '按钮类控件样式设置',
+          'url': '02-button.html'
+        },
+        {
+          'title': '按钮类控件常见样式',
+          'url': '03-button.html'
+        },
+        {
+          'title': '文本框控件',
+          'url': '11-textbox.html'
+        },
+        {
+          'title': '密码强度检测',
+          'url': '12-password.html'
+        },
+        {
+          'title': '密码可见',
+          'url': '13-password.html'
+        },
+        {
+          'title': '弹框',
+          'url': '21-dialog.html'
+        },
+        {
+          'title': '范围控件',
+          'url': '31-range.html'
+        },
+        {
+          'title': '进度条控件',
+          'url': '41-progress.html'
+        }
+      ]
+    },
+    {
+      'class': '数据合法性校验',
+      'baseUrl': '02-validation',
+      'demos': [
+        {
+          'title': '字段级验证',
+          'url': '00-field-validation.html'
+        },
+        {
+          'title': '字符级验证',
+          'url': '01-char-validation.html'
+        },
+        {
+          'title': '表单级验证',
+          'url': '02-form-validation.html'
+        },
+        {
+          'title': 'H5 验证',
+          'url': '03-h5-validation.html'
+        }
+      ]
+    },
+    {
+      'class': '第三方组件库',
+      'baseUrl': '03-third-party-widget',
+      'demos': [
+        {
+          'title': '二进熵函数',
+          'url': '01-echart.html'
+        }
+      ]
+    },
+    {
+      'class': 'MVC 和数据绑定',
+      'baseUrl': '06-mvc',
+      'demos': [{
+        'title': '控制方块颜色和大小',
+        'url': 'index.html'
+      }]
+    },
+    {
+      'class': '数据存储',
+      'baseUrl': '07-storage',
+      'demos': [
+        {
+          'title': '按钮状态保持 1',
+          'url': '01-local-storage.html'
+        },
+        {
+          'title': '按钮状态保持 2',
+          'url': '02-history.html'
+        },
+        {
+          'title': 'LocalStorage 保存图片',
+          'url': '03-image-to-local-storage.html'
+        }
+      ]
+    },
+    {
+      'class': '多媒体',
+      'baseUrl': '08-multimedia',
+      'demos': [
+        {
+          'title': '自制滑杆控件',
+          'url': '01-range.html'
+        }
+      ]
+    }
+  ];
